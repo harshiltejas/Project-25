9b7365efdf5dc5666e4e4795b5429680c0ef03cc
harshiltejas <83020849+harshiltejas@users.noreply.github.com>
2021-05-24 20:08:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject,paper;
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);       
+
+    
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	paper = new Paper(400,325,80,80)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+  keyPressed();
+  groundObject.display();
+  dustbinObj.display();
+  paper.display();
+
+}
+
+function keyPressed(){
+if(keyCode === UP_ARROW){
+paper.y = paper.y-20;
+}
+
+if(keyCode === DOWN_ARROW){
+	paper.y = paper.y+20;
+	}
+
+if(keyCode === RIGHT_ARROW){
+	paper.x = paper.x+20;
+	}
+
+if(keyCode === LEFT_ARROW){
+	paper.y = paper.y+20;
+	}
+
+
+}
+
+
+
